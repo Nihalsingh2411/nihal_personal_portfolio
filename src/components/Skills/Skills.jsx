080ad6812764b4e647a60ed51a694007218a297b
Nihal Singh Bhadoriya <166745563+2411nihalsingh@users.noreply.github.com>
2025-05-29 10:36:58 +0000

    skills section done

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -6,7 +6,7 @@ import Tilt from "react-parallax-tilt";
 const Skills = () => (
   <section
     id="skills"
-    className="py-24 pb-24 px-[12vw] md:px-[7vw] lg:px-[18vw] font-sans bg-skills-gradient clip-path-custom"
+    className="py-24 pb-24 px-[12vw] md:px-[7vw] lg:px-[16vw] font-sans bg-skills-gradient clip-path-custom"
   >
     {/* Section Title */}
     <div className="text-center mb-8">
@@ -22,7 +22,7 @@ const Skills = () => (
       {SkillsInfo.map((category) => (
         <div
           key={category.title}
-          className="bg-gray-900 backdrop-blur-md px-6 sm:px-10 py-8 sm:py-6 mb-10 w-full sm:w-[48%] rounded-2xl border border-white 
+          className="bg-gray-900 backdrop-blur-md px-6 sm:px-6 py-8 sm:py-6 mb-10 w-full sm:w-[48%] rounded-2xl border border-white 
           shadow-[0_0_20px_1px_rgba(130,69,236,0.3)]"
         >
           <h3 className="text-2xl sm:text-3xl font-semibold text-gray-400 mb-4 text-center">
--- src/constants.js
+++ src/constants.js
@@ -1,37 +1,37 @@
 // Skills Section Logo's
+import tensorflowLogo from "./assets/tech_logo/tensorflowLogo.webp";
+import kerasLogo from "./assets/tech_logo/keras.png";
+import pytorchLogo from "./assets/tech_logo/pytorch.png";
+import sklearnLogo from "./assets/tech_logo/sklearn.png";
+import huggingfaceLogo from "./assets/tech_logo/huggingface.svg";
+import pandasLogo from "./assets/tech_logo/pandaslogo.png";
+import excelLogo from "./assets/tech_logo/msexcel.png";
+import matplotlibLogo from "./assets/tech_logo/matplotliblogo.png";
+import jupyterLogo from "./assets/tech_logo/jupyter.png";
+import NumpyLogo from "./assets/tech_logo/Numpy.png";
+import seabornLogo from "./assets/tech_logo/seaborn.svg";
+import powerbiLogo from "./assets/tech_logo/powerbi.png";
+import geminiLogo from "./assets/tech_logo/gemini.png";
+import flasklogo from "./assets/tech_logo/flasklogo.png";
+import googlecolablogo from "./assets/tech_logo/googlecolab.png";
+import fastapilogo from "./assets/tech_logo/fastapi.png";
+import streamlitlogo from "./assets/tech_logo/streamlitlogo.png";
+import rlogo from "./assets/tech_logo/r.jpg";
 import htmlLogo from './assets/tech_logo/html.png';
 import cssLogo from './assets/tech_logo/css.png';
-import sassLogo from './assets/tech_logo/sass.png';
 import javascriptLogo from './assets/tech_logo/javascript.png';
-import reactjsLogo from './assets/tech_logo/reactjs.png';
-import angularLogo from './assets/tech_logo/angular.png';
-import reduxLogo from './assets/tech_logo/redux.png';
-import nextjsLogo from './assets/tech_logo/nextjs.png';
-import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
-import gsapLogo from './assets/tech_logo/gsap.png';
-import materialuiLogo from './assets/tech_logo/materialui.png';
-import bootstrapLogo from './assets/tech_logo/bootstrap.png';
-import springbootLogo from './assets/tech_logo/springboot.png';
-import nodejsLogo from './assets/tech_logo/nodejs.png';
-import expressjsLogo from './assets/tech_logo/express.png';
 import mysqlLogo from './assets/tech_logo/mysql.png';
 import mongodbLogo from './assets/tech_logo/mongodb.png';
-import firebaseLogo from './assets/tech_logo/firebase.png';
 import cLogo from './assets/tech_logo/c.png';
 import cppLogo from './assets/tech_logo/cpp.png';
-import javaLogo from './assets/tech_logo/java.png';
 import pythonLogo from './assets/tech_logo/python.png';
-import typescriptLogo from './assets/tech_logo/typescript.png';
 import gitLogo from './assets/tech_logo/git.png';
 import githubLogo from './assets/tech_logo/github.png';
 import vscodeLogo from './assets/tech_logo/vscode.png';
 import postmanLogo from './assets/tech_logo/postman.png';
-import mcLogo from './assets/tech_logo/mc.png';
-import figmaLogo from './assets/tech_logo/figma.png';
-import netlifyLogo from './assets/tech_logo/netlify.png';
 import vercelLogo from './assets/tech_logo/vercel.png';
-import postgreLogo from './assets/tech_logo/postgre.png';
-import csharpLogo from './assets/tech_logo/csharp.png';
+import langchainLogo from './assets/tech_logo/langchain.svg'; 
+import openCVLogo from './assets/tech_logo/opencv.png'; 
 
 // Experience Section Logo's
 import webverseLogo from './assets/company_logo/webverse_logo.png';
@@ -57,32 +57,31 @@ import removebgLogo from './assets/work_logo/remove_bg.png';
 
 export const SkillsInfo = [
   {
-    title: 'Frontend',
+    title: 'Data Science',
     skills: [
-      { name: 'HTML', logo: htmlLogo },
-      { name: 'CSS', logo: cssLogo },
-      { name: 'SASS', logo: sassLogo },
-      { name: 'JavaScript', logo: javascriptLogo },
-      { name: 'React JS', logo: reactjsLogo },
-      { name: 'Angular', logo: angularLogo },
-      { name: 'Redux', logo: reduxLogo },
-      { name: 'Next JS', logo: nextjsLogo },
-      { name: 'Tailwind CSS', logo: tailwindcssLogo },
-      { name: 'GSAP', logo: gsapLogo },
-      { name: 'Material UI', logo: materialuiLogo },
-      { name: 'Bootstrap', logo: bootstrapLogo },
+      { name: 'Python', logo: pythonLogo },
+      { name: 'Pandas', logo: pandasLogo },
+      { name: 'Numpy', logo: NumpyLogo },
+      { name: 'Matplotlib', logo: matplotlibLogo },
+      { name: 'Seaborn', logo: seabornLogo },
+      { name: 'Jupyter Notebook', logo: jupyterLogo },
+      { name: 'Excel', logo:excelLogo},
+      { name: 'PowerBI', logo: powerbiLogo },
+      
     ],
   },
   {
-    title: 'Backend',
+    title: 'AI & Machine Learning',
     skills: [
-      { name: 'Springboot', logo: springbootLogo },
-      { name: 'Node JS', logo: nodejsLogo },
-      { name: 'Express JS', logo: expressjsLogo },
-      { name: 'MySQL', logo: mysqlLogo },
-      { name: 'MongoDB', logo: mongodbLogo },
-      { name: 'Firebase', logo: firebaseLogo },
-      { name: 'PostgreSQL', logo: postgreLogo },
+      { name: 'Tensorflow', logo: tensorflowLogo },
+      { name: 'Keras', logo: kerasLogo },
+      { name: 'Pytorch', logo: pytorchLogo },
+      { name: 'Scikit-learn', logo: sklearnLogo },
+      { name: 'OpenCV', logo: openCVLogo},
+      { name: 'Hugging Face', logo: huggingfaceLogo },
+      { name: 'LangChain', logo: langchainLogo },
+      { name: 'GeminiAPI', logo: geminiLogo },
+      { name: 'Flask', logo: flasklogo },
     ],
   },
   {
@@ -90,11 +89,13 @@ export const SkillsInfo = [
     skills: [
       { name: 'C', logo: cLogo },
       { name: 'C++', logo: cppLogo },
-      { name: 'Java', logo: javaLogo },
       { name: 'Python', logo: pythonLogo },
-      { name: 'C-Sharp', logo: csharpLogo },
       { name: 'JavaScript', logo: javascriptLogo },
-      { name: 'TypeScript', logo: typescriptLogo },
+      { name: 'R', logo: rlogo },
+      { name: 'MySQL', logo: mysqlLogo },
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+      
     ],
   },
   {
@@ -104,10 +105,11 @@ export const SkillsInfo = [
       { name: 'GitHub', logo: githubLogo },
       { name: 'VS Code', logo: vscodeLogo },
       { name: 'Postman', logo: postmanLogo },
-      { name: 'Compass', logo: mcLogo },
+      { name: 'Google colab', logo: googlecolablogo},
       { name: 'Vercel', logo: vercelLogo },
-      { name: 'Netlify', logo: netlifyLogo },
-      { name: 'Figma', logo: figmaLogo },
+      { name: 'Streamlit', logo: streamlitlogo },
+      { name: 'FastAPI', logo: fastapilogo },
+      { name: 'MongoDB', logo: mongodbLogo },
     ],
   },
 ];
@@ -116,21 +118,21 @@ export const SkillsInfo = [
     {
       id: 0,
       img: webverseLogo,
-      role: "Fullstack Developer",
-      company: "Webverse Digital",
-      date: "April 2024 - Present",
-      desc: "Developed dynamic and scalable web applications using the MERN stack, handling both frontend and backend development. Collaborated with cross-functional teams to build responsive UI, implement RESTful APIs, and optimize application performance in an agile environment.",
+      role: "Software Engineer Intern",
+      company: "Corecard Software India Pvt. Ltd. Bhopal",
+      date: " Jun 2024 - Jul 2024",
+      desc: "Rebuilt the company website using the MERN stack, achieving 30% faster load times and enhanced performance. Developed 5+ responsive UI components and resolved 30+ front-end bugs to boost stability. Collaborated in design reviews to improve overall user experience.",
       skills: [
         "HTML",
         "CSS",
+        "Bootstrap",
         "JavaScript",
         "React JS",
-        "TypeScript",
-        "Node JS",
-        "Tailwind CSS",
-        "MongoDb",
-        "Redux",
-        " Next Js",
+        "UI/UX",
+        "REST API",
+        "Postman",
+        "A/B Testing",
+        "Font Awesome",
       ],
     },
     {
